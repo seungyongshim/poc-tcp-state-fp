807e8f4b34b63cad4e6a2a7420bde6b17d7dfed6
seungyongshim <seungyongshim@gmail.com>
2024-05-26 13:42:59 +0900

    .

--- src/Sample/Class1.cs
+++ src/Sample/Class1.cs
@@ -2,27 +2,4 @@ using System.Net.NetworkInformation;
 
 namespace Sample;
 
-public class TcpConnection
-{
-    public TcpState1 State { get; internal set; }
-
-
-    public TcpConnection()
-    {
-        State = TcpClosed.Instance;
-    }
-
-    public string Host { get; }
-    public int Port { get; }
-}
-
-public class TcpClosed : TcpState1
-{
-    public static TcpClosed Instance { get; } = new TcpClosed();
-
-    private TcpClosed()
-    {
-    }
-
-    public override void ActiveOpen(TcpConnection connection) => base.ActiveOpen(connection);
-}
+public TcpState
