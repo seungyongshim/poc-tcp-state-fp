589956b457da820d2cc6673fd9f51716eb6263ce
seungyongshim <seungyongshim@gmail.com>
2024-05-26 13:39:39 +0900

    .

--- src/Sample/Class1.cs
+++ src/Sample/Class1.cs
@@ -1,7 +1,28 @@
+using System.Net.NetworkInformation;
+
 namespace Sample;
 
-public static class Prelude
+public class TcpConnection
 {
-    public static Func<int, Func<int, int>> add =>
-        x => y => x + y;
+    public TcpState1 State { get; internal set; }
+
+
+    public TcpConnection()
+    {
+        State = TcpClosed.Instance;
+    }
+
+    public string Host { get; }
+    public int Port { get; }
+}
+
+public class TcpClosed : TcpState1
+{
+    public static TcpClosed Instance { get; } = new TcpClosed();
+
+    private TcpClosed()
+    {
+    }
+
+    public override void ActiveOpen(TcpConnection connection) => base.ActiveOpen(connection);
 }
---
+++ src/Sample/TcpState1.cs
@@ -0,0 +1,31 @@
+﻿namespace Sample;
+
+public class TcpState1
+{
+    public virtual void Transmit(TcpConnection connection, TcpOctetstream stream)
+    {
+    }
+
+    public virtual void ActiveOpen(TcpConnection connection)
+    {
+    }
+    public virtual void PassiveOpen(TcpConnection connection)
+    {
+    }
+
+    public virtual void Close(TcpConnection connection)
+    {
+    }
+
+    public virtual void Synchronize(TcpConnection connection)
+    {
+    }
+
+    public virtual void Acknowledge(TcpConnection connection)
+    {
+    }
+
+    public virtual void Send(TcpConnection connection)
+    {
+    }
+}
